408205abbdcda390e21f7e3620f8abd620eb3859
Rahmi <magicra84@gmail.com>
2020-08-08 14:46:25 -0300

    my local settar branch is done

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
       System.out.println("our first commit from local repo");
       System.out.println("our second commit is on the way from local repository");
+      System.out.println("now i have my own Rahmi branch maaaan");
 	}
 
 }
