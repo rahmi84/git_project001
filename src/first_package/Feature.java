39d7805e56516a20774623736cabc062588962a5
rahmi84 <69377204+rahmi84@users.noreply.github.com>
2020-08-08 16:59:20 -0300

    Update Feature.java
    the second commit from remote master branch

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -4,7 +4,9 @@ public class Feature {
 
 	public static void main(String[] args) {
     System.out.println("this is my commit from feature branch");
-		System.out.println("this is the first commit from remote master branches");
+    System.out.println("this is the first commit from remote master branches");
+     System.out.println("this is the second commit from remote master branches");
+    
 		
 	}
 
